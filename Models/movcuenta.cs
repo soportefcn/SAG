3adeb27175599912bd4890e9152b1bb97596d751
shascon <pherrera@ciudaddelnino.cl>
2022-06-07 17:26:53 -0400

    Modelo Informe cuenta Consolidado
    Modelo cuenta consolidado

--- Models/movcuenta.cs
+++ Models/movcuenta.cs
@@ -21,4 +21,23 @@ namespace SAG2.Models
         public string NombreCuenta { get; set; }
 
     }
+    public class movcuentatr
+    {
+        public string Tipo { get; set; }
+        public string Region { get; set; }
+        public string codCodeni { get; set; }
+        public int ProyectoID { get; set; }
+        public int TipoComprobanteID { get; set; }
+        public int IDComprobante { get; set; }
+        public int NumeroComprobante { get; set; }
+        public int Periodo { get; set; }
+        public int Mes { get; set; }
+        public DateTime? Fecha { get; set; }
+        public int Monto { get; set; }
+        public string CtaCte { get; set; }
+        public string Glosa { get; set; }
+        public int? Cheque { get; set; }
+        public string NombreCuenta { get; set; }
+
+    }
 }
