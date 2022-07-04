58d6cb58fd4ac031b8fa0e1734d11c42dc585126
shascon <pherrera@ciudaddelnino.cl>
2022-07-04 10:07:11 -0400

    Produccion Revision

--- Controllers/SenainfoController.cs
+++ Controllers/SenainfoController.cs
@@ -21,12 +21,11 @@ namespace SAG2.Controllers
             int largoCodigoSename = 7;
             ViewBag.Proyec = db.Proyecto.Where(p => p.CodSename.Length != largoCodigoSename && p.Cerrado == null && p.Eliminado == null);
             ViewBag.Proy = db.Proyecto.Where(p => p.CodSename.Length == null && p.Cerrado == null && p.Eliminado == null);
-            ViewBag.TipoSename = db.TipoSename.ToList() ;
             return View();
         }
 
         [HttpPost]
-        public ActionResult ExportarArchivo(int periodo, int mes, int TipoSename )
+        public ActionResult ExportarArchivo(int periodo, int mes)
         {
             /*int periodo = (int)Session["Periodo"];
             int mes = (int)Session["Mes"];*/
@@ -39,19 +38,17 @@ namespace SAG2.Controllers
         }
 
         [HttpPost]
-        public ActionResult Exportar(int periodo, int mes, int TipoSename)
+        public ActionResult Exportar(int periodo, int mes)
         {
             ViewBag.mes = mes;
             ViewBag.periodo = periodo;
             ViewBag.Movimientos = db.Movimiento.Where(m => m.auto == 0).Where(m => m.Periodo == periodo).Where(m => m.Mes == mes).Where(a => a.Temporal == null && a.Eliminado == null && ((a.CuentaID != 1 && a.CuentaID != 6) || a.CuentaID == null)).OrderBy(m => m.ProyectoID).ThenBy(m => m.TipoComprobanteID).ThenBy(m => m.NumeroComprobante).ToList();
-            ViewBag.sigla = db.TipoSename.Where(m => m.ID == TipoSename).FirstOrDefault().Sigla;    
+
             // Largo del codigo de Sename
             int largoCodigoSename = 7;
-            // agregar filtro segun corresponda
-        //    var Proyectos = db.Proyecto.Where(p => p.Cerrado == null && p.Eliminado == null && p.CodSename != null && !p.CodSename.Equals("") && p.CodSename.Length == largoCodigoSename && p.TipoProyecto.LineaAtencion.TipoSenameID == TipoSename).OrderBy(p => p.ID);
+
             var Proyectos = db.Proyecto.Where(p => p.Cerrado == null && p.Eliminado == null && p.CodSename != null && !p.CodSename.Equals("") && p.CodSename.Length == largoCodigoSename).OrderBy(p => p.ID);
-           
-            // var Proyectos = db.Proyecto.Where(p => p.ID == 148);
+           // var Proyectos = db.Proyecto.Where(p => p.ID == 148);
             return View(Proyectos.ToList());
         }
 
